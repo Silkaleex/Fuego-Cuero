c3f4a511f4532d9c2e4735ba4263fae2f582ac3a
aleja@Silkaleex <alejandropascual1995@outlook.es>
2023-12-02 13:41:35 +0100

    Fuego&cuero

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 class Database{
     public static function connect(){
-        $db = new mysqli("localhost","root","3i7Ka7330Ba63D3dA10s","tienda");
+        $db = new mysqli("localhost","root","","tienda");
         $db->query("SET NAMES 'UTF8'");
         return $db;
     }
